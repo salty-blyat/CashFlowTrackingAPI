4ff1b370736659fc25765be2ca1f7a7628a705d5
salty-blyat <hokheng12123@gmail.com>
2024-06-29 03:23:55 +0700

    fix port

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const cors = require('cors');
 const middlewares = jsonServer.defaults();
-
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
-server.use(cors)
 
 server.listen(port);
